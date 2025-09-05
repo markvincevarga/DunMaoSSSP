f6cc1a18692a08bb4abd64d70e3b8171252a584e
Márk Vince Varga <mark.vince.varga@gmail.com>
2025-09-05 16:35:14 +0200

    add graph caching for OSM

--- tests/graph_loader.rs
+++ tests/graph_loader.rs
@@ -11,6 +11,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
+const CACHE_PATH: &str = "./data/cache";
+
 #[cfg(feature = "flate2")]
 use flate2::read::GzDecoder;
 use tqdm::tqdm;
@@ -163,82 +165,89 @@ pub fn read_wiki_graph_for_fast_sssp(gz_path: &Path) -> Graph {
 }
 
 pub fn read_osm_pbf_map(pbf_path: &Path) -> Graph {
-    let reader = ElementReader::from_path(pbf_path).expect("OSM PBF file should exist");
+    let filename = pbf_path.file_name().expect("Path should have a filename");
+    let cache_path = Path::new(CACHE_PATH).join(Path::new(filename).with_extension("cache"));
+    let graph = Graph::from_file(&cache_path).unwrap_or_else(|_| {
+        let reader = ElementReader::from_path(pbf_path).expect("OSM PBF file should exist");
 
-    struct OsmNode {
-        idx: usize,
-        lat: f64,
-        lon: f64,
-    }
+        struct OsmNode {
+            idx: usize,
+            lat: f64,
+            lon: f64,
+        }
 
-    struct OsmEdge {
-        from: i64,
-        to: i64,
-    }
+        struct OsmEdge {
+            from: i64,
+            to: i64,
+        }
 
-    let mut nodes = HashMap::new();
-    let mut edges = Vec::new();
-    let mut idx = 0;
-    reader
-        .for_each(|element| {
-            match element {
-                Element::Node(node) => {
-                    nodes.insert(
-                        node.id(),
-                        OsmNode {
-                            idx: idx,
-                            lat: node.lat(),
-                            lon: node.lon(),
-                        },
-                    );
-                    idx += 1;
-                }
-                Element::DenseNode(node) => {
-                    nodes.insert(
-                        node.id(),
-                        OsmNode {
-                            idx: idx,
-                            lat: node.lat(),
-                            lon: node.lon(),
-                        },
-                    );
-                    idx += 1;
-                }
-                Element::Way(way) => {
-                    let ways_iter_clone = way.refs().clone();
-                    for (a, b) in way.refs().zip(ways_iter_clone.skip(1)) {
-                        edges.push(OsmEdge { from: a, to: b })
+        let mut nodes = HashMap::new();
+        let mut edges = Vec::new();
+        let mut idx = 0;
+        reader
+            .for_each(|element| {
+                match element {
+                    Element::Node(node) => {
+                        nodes.insert(
+                            node.id(),
+                            OsmNode {
+                                idx: idx,
+                                lat: node.lat(),
+                                lon: node.lon(),
+                            },
+                        );
+                        idx += 1;
                     }
-                }
-                _ => {}
-            };
-        })
-        .expect("file must include correct osm data");
+                    Element::DenseNode(node) => {
+                        nodes.insert(
+                            node.id(),
+                            OsmNode {
+                                idx: idx,
+                                lat: node.lat(),
+                                lon: node.lon(),
+                            },
+                        );
+                        idx += 1;
+                    }
+                    Element::Way(way) => {
+                        let ways_iter_clone = way.refs().clone();
+                        for (a, b) in way.refs().zip(ways_iter_clone.skip(1)) {
+                            edges.push(OsmEdge { from: a, to: b })
+                        }
+                    }
+                    _ => {}
+                };
+            })
+            .expect("file must include correct osm data");
 
-    let mut graph = Graph::new(nodes.len());
+        let mut graph = Graph::new(nodes.len());
 
-    println!(
-        "Number of nodes: {:}, Number of edges: {:}",
-        nodes.len().to_formatted_string(&Locale::is),
-        edges.len().to_formatted_string(&Locale::is)
-    );
-    tqdm(edges.iter()).for_each(|edge| {
-        let node_a = &nodes[&edge.from];
-        let node_b = &nodes[&edge.to];
-        let distance = Haversine.distance(
-            Point::new(node_a.lon, node_a.lat),
-            Point::new(node_b.lon, node_b.lat),
+        println!(
+            "Number of nodes: {:}, Number of edges: {:}",
+            nodes.len().to_formatted_string(&Locale::is),
+            edges.len().to_formatted_string(&Locale::is)
         );
-        graph.add_edge(node_a.idx, node_b.idx, distance);
-        graph.add_edge(node_b.idx, node_a.idx, distance);
-    });
+        tqdm(edges.iter()).for_each(|edge| {
+            let node_a = &nodes[&edge.from];
+            let node_b = &nodes[&edge.to];
+            let distance = Haversine.distance(
+                Point::new(node_a.lon, node_a.lat),
+                Point::new(node_b.lon, node_b.lat),
+            );
+            graph.add_edge(node_a.idx, node_b.idx, distance);
+            graph.add_edge(node_b.idx, node_a.idx, distance);
+        });
 
+        std::fs::create_dir_all(CACHE_PATH).expect("Failed to create cache directory");
+        Graph::to_file(&graph, &cache_path).expect("saving to cache file should succeed");
+
+        graph
+    });
     println!(
         "Number of edges: {:}",
         graph.edge_count().to_formatted_string(&Locale::is)
     );
-
-    return graph;
+    graph
 }
 
 pub fn convert_to_petgraph(graph: &Graph) -> (DiGraph<(), f64>, HashMap<usize, NodeIndex>) {
