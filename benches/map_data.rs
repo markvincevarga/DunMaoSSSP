fef26dcf1b8d74ef8e321ded47da5821fa4aa1d6
Márk Vince Varga <mark.vince.varga@gmail.com>
2025-09-10 15:36:31 +0200

    remove petgraph dijsktra due to slow performance

--- benches/map_data.rs
+++ benches/map_data.rs
@@ -3,8 +3,6 @@ use fast_sssp::DuanMaoSolverV2;
 use fast_sssp::algo::dijkstra as own_dijkstra;
 use fast_sssp::algo::dijkstra_fibonacci as own_dijkstra_fib;
 use fast_sssp::graph::Graph;
-use petgraph::algo::dijkstra;
-use petgraph::graph::DiGraph;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 use std::hint::black_box;
@@ -17,16 +15,6 @@ fn run_fast_sssp_sequential(graph: &Graph, pairs: &[(usize, usize)]) {
     let mut solver = DuanMaoSolverV2::new(graph.clone());
     for (source, goal) in pairs {
         black_box(solver.solve(*source, *goal));
-    }
-}
-
-fn run_petgraph_dijkstra(graph: &DiGraph<(), f64>, pairs: &[(usize, usize)]) {
-    for (source, goal) in pairs {
-        let source_node = graph.node_indices().find(|i| i.index() == *source).unwrap();
-        let goal_node = graph.node_indices().find(|i| i.index() == *goal).unwrap();
-        black_box(dijkstra(&graph, source_node, Some(goal_node), |e| {
-            *e.weight()
-        }));
     }
 }
 
@@ -45,7 +33,6 @@ fn run_own_dijkstra(graph: &Graph, pairs: &[(usize, usize)]) {
 fn benchmark(c: &mut Criterion) {
     let path = Path::new("data/gotland.osm.pbf");
     let fast_sssp_graph = graph_loader::read_osm_pbf_map(path);
-    let (petgraph_graph, _) = graph_loader::convert_to_petgraph(&fast_sssp_graph);
     let own_graph = fast_sssp_graph.clone();
     let own_graph_fib = fast_sssp_graph.clone();
 
@@ -71,10 +58,6 @@ fn benchmark(c: &mut Criterion) {
 
     group.bench_function("fast_sssp_sequential", |b| {
         b.iter(|| run_fast_sssp_sequential(black_box(&fast_sssp_graph), black_box(&pairs)))
-    });
-
-    group.bench_function("petgraph_dijkstra", |b| {
-        b.iter(|| run_petgraph_dijkstra(black_box(&petgraph_graph), black_box(&pairs)))
     });
 
     group.finish();
